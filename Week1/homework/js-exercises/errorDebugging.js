8a5a4f90875277508b07cb5ebc4c1b169c0ec714
Hettinia <abedhettini@gmail.com>
2020-01-31 17:49:07 +0200

    Update the branch, now just contain week2 homework

--- Week1/homework/js-exercises/errorDebugging.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-
-//the statement was: console.log('I'm awesome');
-console.log("I'm awesome");
--- Week1/homework/js-exercises/logStringLength.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-//Declare string variable
-let mySentence = "Programming is so interesting!";
-
-//Calculate the string lenght
-let mySentenceLength = mySentence.length;
-console.log(mySentenceLength);
